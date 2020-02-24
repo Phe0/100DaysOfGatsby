ce8eba8c638addd6ac4ae24a5cb7f8eda4297608
phe0 <pheofo@gmail.com>
2020-02-23 22:05:05 -0300

    add footer to page

--- src/components/layout.js
+++ src/components/layout.js
@@ -19,16 +19,30 @@ export default ({ children }) => {
   return (
     <div className={style.layout}>
       <div className={style.linkArea}>
-        <Link to={`/`} style={{ textDecoration: "none"}}>
+        <Link to={`/`} style={{ textDecoration: "none" }}>
           <div className={style.linkSection}>
             <div className={style.link}>
               <h3 className={style.title}>{data.site.siteMetadata.title}</h3>
-              <img className={style.iconImg} src={gatsbyIcon} alt="Gatsby Icon" />
+              <img
+                className={style.iconImg}
+                src={gatsbyIcon}
+                alt="Gatsby Icon"
+              />
             </div>
           </div>
         </Link>
       </div>
-      {children}
+      <div className={style.content}>
+        {children}
+      </div>
+      <div className={style.bottom}>
+        <Link to={`/`} style={{ textDecoration: "none" }}>
+          <h6 className={style.links}>HOME</h6>
+        </Link>
+        <Link to={`/about`} style={{ textDecoration: "none" }}>
+          <h6 className={style.links}>ABOUT ME</h6>
+        </Link>
+      </div>
     </div>
   )
 }
